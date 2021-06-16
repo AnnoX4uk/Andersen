9623e62c7a6e4c117497f055c66b1b84ec142fef
annox <annox4uk@gmail.com>
2021-06-16 16:29:41 +0700

    improve netstat_scr *add output for not found connections *add functional for netstat

--- netstat_script/netstat_scr.sh
+++ netstat_script/netstat_scr.sh
@@ -11,6 +11,7 @@ programm_name=''
 connection='ESTABLISHED'
 out_lines=5
 
+#TODO: rework script to input connection states like WAIT/LISTEN/etc
 print_usage () {
     echo "Usage: netstat_scr -p {programm_name}"
     echo "Optional args:"
@@ -34,7 +35,12 @@ p) programm_name=$OPTARG
 ;;
 a) connection='ALL' 
 ;;
-l) out_lines=$OPTARG
+l) if [[ "$OPTARG" =~ [^0-9]+ ]]; then
+  echo "Incorrent line numbers in -l option. Output $out_lines lines only"
+   else 
+   out_lines=$OPTARG
+fi
+
 ;;
 *) echo "Found invalid option!"
 print_usage;;
@@ -48,5 +54,26 @@ if [[ $programm_name == '-a' ]] | [[ $programm_name == '-l' ]]; then
     exit 1
 fi
 
+#check netstat connections 
+if (( $connection == "ALL" )); then
+    ip="$(netstat -tunapl)"
+else
+    ip="$(netstat -tunapl| grep "$connection")"
+fi
 
-#netstat -tunapl | awk '/firefox/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+
+#check app connections
+ip=$(echo "$ip" | awk '/'"$programm_name"/' {print $5}')
+
+#Exit if blank output
+if [ -z "$ip" ]; then
+     echo "Not found connections for $programm_name"
+     exit 1
+fi
+
+#convert netstat output to simple IP-addresses
+ip=$(echo "$ip" |cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+')
+
+echo $ip
+
+# cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
