442aa13ed9607325430fa5dc3741ef7cc59e2b97
annox <annox4uk@gmail.com>
2021-06-18 12:05:36 +0700

    minor syntax changes

--- ansible_assigment/zoo_app/interface.py
+++ ansible_assigment/zoo_app/interface.py
@@ -1,28 +1,30 @@
 import json
 import emoji
 from flask import Flask, request
-#TODO: make config file for service
-sign='Made with ❤️  by AnnoX4uk\n'
+
+
+# TODO: make config file for service
+sign = 'Made with ❤️  by AnnoX4uk\n'
 
 def emojiout(text):
     r = emoji.emojize(':' + text + ':')
-    return(r.replace(':',''))
+    return(r.replace(':', ''))
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #TODO: make somethink to work with GET
+    # TODO: make somethink to work with GET
     if request.method == "GET":
         return 'Your method is GET\n'
 
     if request.method == "POST":
         print(request.content_type)
         try:
-            data = request.get_json(force = True)
+            data = request.get_json(force=True)
         except Exception as err:
             return('Can not convert your data to json\n')
-        
+
         try:
             if data.keys().isdisjoint(['animal']):
                 return ('Error while parse arguments, animal not found. Please try again.\
@@ -33,22 +35,24 @@ def index():
             if data.keys().isdisjoint(['count']):
                 return ('Error while parse arguments, count not found. Please try again.\
                      format: /{animal:"someanimal", sound:"anysound", count: "anycount"/}\n')
-            ##TODO: need check that animal is animal, sound in a sound
+            # TODO: need check that animal is animal, sound in a sound
             else:
                 try:
                     if int(data['count']) < 1:
                         return (sign)
                 except Exception as err:
-                    print ('Got Error: {} '.format(err))
+                    print('Got Error: {} '.format(err))
                     return ('Server got error, can not convert count to integer\n')
-        
+
         except Exception as err:
-            print ('Got error while rescv json args: {}\n'.format(err))
-        
+            print('Got error while rescv json args: {}\n'.format(err))
+            
         animalsay = ''
-        for i in range (0, int(data['count'])):
-            animalsay = animalsay + '{animal} says {sound}\n'.format(animal = emojiout(data['animal']), sound = data['sound'])
-        
+        for i in range(0, int(data['count'])):
+            animalsay = animalsay + \
+                '{animal} says {sound}\n'.format(
+                    animal=emojiout(data['animal']), sound=data['sound'])
+
         return (animalsay + sign)
 
 
--- netstat_script/netstat_scr.sh
+++ netstat_script/netstat_scr.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-#Simple script writen by AnnoX4uk. Useful for check active connections for some application
-#See README.md for more information or run script without any options. 
+# Simple script writen by AnnoX4uk. Useful for check active connections for some application
+# See README.md for more information or run script without any options.
 
-#check whois
+
+# check whois
 if [[ -z "$(which whois)" ]]; then
     echo "Whois package not installed. Please install whois and try again"
     exit
 fi
 
-#check netstat
+# check netstat
 if [[ -z "$(which netstat)" ]]; then
     echo "Netstat package not installed. Please install netstat and try again"
     exit
 fi
 
-#check privileges before starting
+# check privileges before starting
 if (($EUID != 0)) ; then
     echo "Script running without root privileges"
     echo "If you want to see all connection set sudo before run"
 fi
 
 
-#set variables
+# set variables
 programm_name=''
 variable_connections=(TIME_WAIT ESTABLISHED CLOSE_WAIT LISTEN)
 connection='ESTABLISHED'
@@ -29,6 +30,7 @@ out_lines=5
 debug='OFF'
 userfriendly=False
 whois_info='Organization'
+
 
 print_usage () {
     echo "Usage: netstat_scr -p {programm_name}"
@@ -41,47 +43,48 @@ print_usage () {
     echo "-u : Userfriendly output."
 }
 
-#check arguments before start
-if [ $# -lt 1 ]
-then
-echo "No valid options found."
-print_usage
-exit 1
+
+# check arguments before start
+if [ $# -lt 1 ]; then
+    echo "No valid options found."
+    print_usage
+    exit 1
 fi
 
-#parse arguments
+
+# parse arguments
 while getopts "c:p:l:i:adu" opt
 do
-case $opt in
-p) programm_name=$OPTARG
-;;
-a) connection='ALL' 
-;;
-c)  if [[ " ${variable_connections[*]} " != *" $OPTARG "*  ]]; then
-        echo "Rescieve incorrect connection type, try again"
-        exit 1
-    elif [[ -z "$OPTARG" ]]; then
-        echo "Rescive no special connection states after -c, work with ESTABLISHED connections"
-    else 
-        connection=$OPTARG
-    fi
-;;
-d) debug='ON' 
-;;
-i) whois_info=$OPTARG 
-;;
-u) userfriendly=True 
-;;
-l) if [[ "$OPTARG" =~ [^0-9]+ ]]; then
-  echo "Incorrent line numbers in -l option. Output $out_lines lines only"
-   else 
-   out_lines=$OPTARG
-fi
-
-;;
-*) echo "Found invalid option!"
-print_usage;;
-esac
+    case $opt in
+        p) programm_name=$OPTARG
+        ;;
+        a) connection='ALL'
+        ;;
+        c)  if [[ " ${variable_connections[*]} " != *" $OPTARG "*  ]]; then
+                echo "Rescieve incorrect connection type, try again"
+                exit 1
+            elif [[ -z "$OPTARG" ]]; then
+                echo "Rescive no special connection states after -c, work with ESTABLISHED connections"
+            else
+                connection=$OPTARG
+            fi
+        ;;
+        d) debug='ON'
+        ;;
+        i) whois_info=$OPTARG
+        ;;
+        u) userfriendly=True
+        ;;
+        l) if [[ "$OPTARG" =~ [^0-9]+ ]]; then
+                echo "Incorrent line numbers in -l option. Output $out_lines lines only"
+            else
+                out_lines=$OPTARG
+            fi
+            
+        ;;
+        *) echo "Found invalid option!"
+        print_usage;;
+    esac
 done
 
 if [[ "$debug" == "ON" ]]; then
@@ -90,18 +93,18 @@ if [[ "$debug" == "ON" ]]; then
     out lines: $out_lines\
     whois_info: $whois_info\
     userfriendly: $userfriendly"
-    sleep 1s   
+    sleep 1s
 fi
 
 
-#check programm name for netstat
+# check programm name for netstat
 if [[ $programm_name == '-a' ]] | [[ $programm_name == '-l' ]]; then
     echo "Not found programm name."
     print_usage
     exit 1
 fi
 
-#check netstat connections 
+# check netstat connections
 if (( $connection == "ALL" )); then
     ip="$(netstat -tunapl)"
 else
@@ -115,7 +118,7 @@ if [[ "$debug" == "ON" ]]; then
 fi
 
 
-#check app connections
+# check app connections
 ip=$(echo "$ip" | awk '/'"$programm_name"/' {print $5}')
 
 if [[ "$debug" == "ON" ]]; then
@@ -124,13 +127,13 @@ if [[ "$debug" == "ON" ]]; then
     sleep 1s
 fi
 
-#Exit if blank output
+# Exit if blank output
 if [ -z "$ip" ]; then
-     echo "Not found connections for $programm_name"
-     exit 1
+    echo "Not found connections for $programm_name"
+    exit 1
 fi
 
-#convert netstat output to simple IP-addresses
+# convert netstat output to simple IP-addresses
 ip=$(echo "$ip" |cut -d: -f1 | sort | uniq -c | sort | tail -n"$out_lines" | grep -oP '(\d+\.){3}\d+')
 
 if [[ "$debug" == "ON" ]]; then
@@ -139,7 +142,7 @@ if [[ "$debug" == "ON" ]]; then
     sleep 1s
 fi
 
-#output organzation
+# output organzation
 for addr in $ip; do
     if [[ "$debug" == "ON" ]]; then
         echo "Try do whois to : $addr"
@@ -147,13 +150,13 @@ for addr in $ip; do
     fi
     org=$(whois "$addr")
     if [[ "$debug" == "ON" ]]; then
-      echo "whois output:\
+        echo "whois output:\
         $org"
         sleep 1s
     fi
     res="$(echo "$org" | awk -F ':' '/^'$whois_info'/ {print $2}')"
-
-    #prepare output
+    
+    # prepare output
     if [[ $userfriendly = True ]]; then
         if [[ -z $res ]]; then
             echo "###############################################################"
@@ -170,10 +173,10 @@ for addr in $ip; do
             $whois_info: $res"
             echo "###############################################################"
         fi
-    #default out
+        # default out
     else
         if [[ -z $res ]]; then
-            echo 'Have not information about your request, try another'
+            echo 'Information about your request not found, try another'
         else
             echo $res
         fi
